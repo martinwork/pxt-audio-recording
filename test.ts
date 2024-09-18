2e39659b687d84fd7a7f8a32da210dacf41d8a5a
Martin Williams <martinwork@users.noreply.github.com>
2024-09-18 20:33:18 +0100

    Add status tests

--- test.ts
+++ test.ts
@@ -1,25 +1,30 @@
 input.onButtonPressed(Button.A, function () {
     record.setSampleRate(11000, record.AudioSampleRateScope.Recording)
-    basic.showIcon(IconNames.SmallSquare)
     record.startRecording(record.BlockingState.Blocking)
-    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    // send to serial
-    basic.showIcon(IconNames.Square)
     record.sendToSerial()
-    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     record.setSampleRate(11000, record.AudioSampleRateScope.Playback)
-    basic.showLeds(`
-        . # . . .
-        . # # . .
-        . # # # .
-        . # # . .
-        . # . . .
-        `)
     record.playAudio(record.BlockingState.Blocking)
-    basic.clearScreen()
 })
 serial.setTxBufferSize(254)
+basic.forever(function () {
+    if (record.audioStatus(record.AudioStatus.Playing)) {
+        basic.showLeds(`
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # . .
+            . # . . .
+            `)
+    } else if (record.audioStatus(record.AudioStatus.Recording)) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (record.sendingToSerial()) {
+        // send to serial
+        basic.showIcon(IconNames.Diamond)
+    } else {
+        basic.clearScreen()
+    }
+})
